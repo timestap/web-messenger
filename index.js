ddef3663414bcdf9d247941d0b99171a096b35b4
Kevin Zhang <yingkai.zhang@mail.utoronto.ca>
2016-07-26 23:12:19 +0800

    chat ui working for the most part

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var timestamp = require ("unix-timestamp");
 var jade = require('jade');
 var bodyParser = require('body-parser');
 var json = require('express-json');
+var moment = require('moment');
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -28,32 +29,32 @@ var db = new sqlite3.Database(file);
 
 //Create table 
 db.serialize(function() {
-  if(!exists) {
-    db.run("CREATE TABLE messagetable (msg TEXT, username TEXT, time INT)"); 
-  }
+	if(!exists) {
+	db.run("CREATE TABLE messagetable (msg TEXT, username TEXT, time INT)"); 
+	}
 });
 
 //test username
 var admin = "administrator";
 
 app.get('/', function(req, res){
-  res.send(html_login);
+	res.send(html_login);
 });
 
 app.post('/', function(req, res) {
-    var username = req.body.username;
-    res.cookie('username', username, { httpOnly: false });
-    res.redirect('/chatroom');
+	var username = req.body.username;
+	res.cookie('username', username, { httpOnly: false });
+	res.redirect('/chatroom');
 });
 
 //login page
 app.get('/chatroom', function(req, res){
-  res.sendFile(__dirname + '/main.html');
+	res.sendFile(__dirname + '/main.html');
 });
 
 //serving css files
 app.get('/css/chat.css', function(req, res){
-  res.sendFile(__dirname + '/css/chat.css');
+	res.sendFile(__dirname + '/css/chat.css');
 });
 
 
@@ -63,13 +64,19 @@ io.on('connection', function(socket){
 		console.log('user disconnected');
 	});
 
-	socket.on('chat message', function(msg){
-     io.emit('chat message', msg);
-     console.log(msg)
-     //Now we write to database 
-     var time = timestamp.now();
+	socket.on('chat message', function(msg_obj){
+		console.log(msg_obj);
 
-  //   db.serialize(function() {
+	 //Now we write to database 
+	 var time = timestamp.now();
+	 var time_moment = moment().format('MMMM Do YYYY, h:mm:ss a');
+	 console.log(time_moment);
+	 io.emit('chat message', {message_contents: msg_obj.msg_contents, 
+	 	username: msg_obj.username, 
+	 	cur_time: time_moment
+	 });
+
+	//   db.serialize(function() {
 		// var stmt = db.prepare("INSERT INTO messagetable VALUES (?, ?, ?)");
 		//      stmt.run( msg, admin, time);
 
@@ -85,21 +92,21 @@ io.on('connection', function(socket){
 	 //  	});
 
 
-  //   });
-     
+	//   });
+	 
 
-  });
+	});
 });
 
 process.stdin.resume();//so the program will not close instantly
 
 function exitHandler(options, err) {
-    if (options.cleanup) console.log('clean');
-    if (err) console.log(err.stack);
-    if (options.exit) process.exit();
+	if (options.cleanup) console.log('clean');
+	if (err) console.log(err.stack);
+	if (options.exit) process.exit();
 
-    //close database
-    db.close();
+	//close database
+	db.close();
 }
 
 //do something when app is closing
@@ -112,5 +119,5 @@ process.on('SIGINT', exitHandler.bind(null, {exit:true}));
 process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
 
 http.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });
